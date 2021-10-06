fc1a3eb694a6f9496e3e11a87e0efdb3e866dc2a
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-06 15:57:31 +0100

    added addNote function
    check if the note already exists in notes using filter and push it to the notes array

--- notes.js
+++ notes.js
@@ -1,3 +1,20 @@
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('note added');
+    } else{
+        console.log('note already taken')
+    }
+}
 const saveNotes = function(notes){
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON);
