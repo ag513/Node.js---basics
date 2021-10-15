3c51f5cab09140d130b6b344b8baacc6a5d7dc3f
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-15 15:05:22 +0100

    Object destructuring

--- objects.js
+++ objects.js
@@ -14,3 +14,29 @@ const user2 = {
 }
 
 console.log(user, user2)
+
+
+// Destructuring objects
+
+
+const bike = {
+    label: 'CBR',
+    cc: 649,
+    price: 6500,
+    isNew: false
+}
+
+// const cc = bike.cc;
+// const label = bike.label;
+
+const { label, isNew, cc } = bike;
+const { price: bikePriceInGBP } = bike;
+console.log(label, cc, isNew);
+console.log(bikePriceInGBP, '£')
+
+
+const buyBike = (type, { cc, isNew }) => {
+    console.log(type, cc, isNew)
+}
+
+buyBike('BUY BIKE', bike);
