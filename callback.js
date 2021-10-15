21d349583ac4e9cef985a38c1a704d11b451be4d
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-15 14:46:16 +0100

    Object property shorthand

--- callback.js
+++ callback.js
@@ -16,3 +16,4 @@ const add = (num1, num2, callback) => {
 add(1, 4, (sum) => {
     console.log(sum) // Should print: 5
 })
+
---
+++ objects.js
@@ -0,0 +1,16 @@
+const name = 'Abhinav'
+const age = 25;
+
+
+const user = {
+    name: name,
+    age: age,
+    location: 'England'
+}
+const user2 = {
+    name,
+    age,
+    location: 'Manchester'
+}
+
+console.log(user, user2)
