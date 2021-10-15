bfc24df5cf0c8cbbbb868eb5167f5c1d941decf6
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-15 16:48:28 +0100

    Show different content based on route

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -6,6 +6,12 @@ const app = express();
 app.get('', (req, res) => {
     res.send('Homepage');
 })
+app.get('/about', (req, res) => {
+    res.send('This is about page');
+})
+app.get('/weather', (req, res) => {
+    res.send('This is weather page');
+})
 
 app.listen(3001, () => {
     console.log('server running on port 3001');
